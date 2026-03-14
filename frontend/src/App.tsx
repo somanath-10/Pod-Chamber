2a4d376636f719fa7111b662440fd2ace29ca3ad
somanath-10 <somanathr1006@gmail.com>
2026-03-14 20:00:20 +0530

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex items-center justify-center min-vh-100 bg-gray-100 dark:bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-600 dark:text-blue-400 underline">
+        Tailwind CSS is working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
